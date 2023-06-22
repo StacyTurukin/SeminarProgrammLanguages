e33d6b5643873f394d572cc00755346fb06576e7
Stacy Turukin <ohnotredamn@gmail.com>
2023-06-22 21:39:22 +0300

    все задачки с семинара

--- task_04/Program.cs
+++ task_04/Program.cs
@@ -2,6 +2,13 @@
 и на выходе показывает последнюю цифру этого числа.*/
 
 Console.WriteLine("Введите ваше число:");
-int number =Convert.ToInt32(Console.ReadLine());
-int count = number % 10;
+
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+if ( number < 100 || number > 999) Console.WriteLine("Нужно трехзначное число!");
+else {
+    count = number % 10;
+Console.WriteLine(count);
+}
+
  
