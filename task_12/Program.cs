2a94d37c624e96a629f0e72470f6cc052f64f88a
Stacy Turukin <ohnotredamn@gmail.com>
2023-06-26 13:58:02 +0300

    Задача 12

---
+++ task_12/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Напишите программу, которая будет принимать на вход два числа и выводить, 
+является ли второе число кратным первому. 
+Если второе число некратно первому, то программа выводит остаток от деления.*/
+
+Console.WriteLine("Введите первое число:");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+int result = firstNumber % secondNumber;
+
+if (result == 0)
+{
+    Console.WriteLine("True");
+}
+else
+{
+    Console.WriteLine(result);
+}
