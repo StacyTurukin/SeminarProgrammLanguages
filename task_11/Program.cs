bab0d6e3bbfda58dc9b76e081422ba2f002f00ed
Stacy Turukin <ohnotredamn@gmail.com>
2023-06-26 13:54:54 +0300

    11 задача

---
+++ task_11/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Напишите программу, которая выводит случайное 
+трёхзначное число и удаляет вторую цифру этого числа.*/
+
+int randomNumber = new Random().Next(100,1000);
+Console.WriteLine($"Рандомное-{randomNumber}");
+
+int numberOne = randomNumber % 100;
+int numberTwo = randomNumber / 100;
+
+//Console.Write(numberOne.ToString()+numberTwo.ToString());
+
+Console.Write(numberOne);
+Console.Write(numberTwo);
