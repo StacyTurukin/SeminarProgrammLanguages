f4efd7b3979f81078c3a969d39aa66fa8d0b04f5
Stacy Turukin <ohnotredamn@gmail.com>
2023-06-26 23:55:53 +0300

    задача со стрингом вместо методов

---
+++ dz_task_13/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6*/
+
+Console.Clear();
+Console.WriteLine("Введдите ваше число;");
+string str = Console.ReadLine();
+//int n = str[2];
+if (str.Length > 2 )
+    Console.WriteLine(str[2]);
+else 
+    Console.WriteLine("Третьей цифры нет");
