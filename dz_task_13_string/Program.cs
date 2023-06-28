00c762f2b8487633fe6aa2b2386a5ae775e0d642
Stacy Turukin <ohnotredamn@gmail.com>
2023-06-28 18:58:14 +0300

    13-я задача, решенная через стринг

---
+++ dz_task_13_string/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6*/
+
+Console.Clear();
+Console.WriteLine("Введдите ваше число;");
+string str = Console.ReadLine();
+
+while (str.Length <= 2 )
+{
+    Console.WriteLine("Третьей цифры нет,введите трехзначное или большее число:");
+    str = Console.ReadLine();
+}
+Console.WriteLine($"Ответ: {str[2]}");
+
