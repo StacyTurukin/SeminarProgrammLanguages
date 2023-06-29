6121ee6f5d1c670bf62176dd6566047bd7089aec
Stacy Turukin <ohnotredamn@gmail.com>
2023-06-29 15:16:52 +0300

    Добавил задачку с поиском выходного

---
+++ dz_task_15/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Напишите программу, которая принимает на вход цифру, 
+обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет*/
+
+
+Console.WriteLine("ВВедите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+while ( number < 1 || number > 7 )
+{
+    Console.WriteLine("Введите число от 1 до 7");
+    number = Convert.ToInt32(Console.ReadLine());
+}
+
+if (number > 5 && number <= 7)
+{
+    Console.WriteLine("Выходной");
+}
+else 
+    Console.WriteLine("К сожалению, нужно трудиться");
