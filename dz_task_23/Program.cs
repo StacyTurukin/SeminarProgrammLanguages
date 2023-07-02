574761fd4f18830edd0c629ddbac74c75bd4cd38
Stacy Turukin <ohnotredamn@gmail.com>
2023-07-03 01:43:26 +0300

    Задача с выводом кубов, решенная через do,while

---
+++ dz_task_23/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125*/
+int index = 1;
+Console.WriteLine("Введите число:");
+int value = Convert.ToInt32(Console.ReadLine());
+
+do
+{
+    int result = index * index * index;
+    index = index + 1;
+    Console.Write(result);
+    Console.Write(", ");
+} while (index < value);
+
+if (index == value)
+    Console.Write(Math.Pow(value, 3));
+    Console.WriteLine(".");
+    
