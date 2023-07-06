80778ef5abeec51d4b2010d4950da5dd57d27fd5
Stacy Turukin <ohnotredamn@gmail.com>
2023-07-06 03:25:59 +0300

    Задача с суммой цифр введенного числа

---
+++ dz_task_27/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+ Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+
+Console.WriteLine("Введите ваше число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+do
+{
+    sum += number % 10;
+    number /= 10;
+} while (number != 0);
+Console.WriteLine($"Сумма цифр-{sum}");
