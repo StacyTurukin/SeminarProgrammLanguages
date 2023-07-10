d584526e1efed624c2d5b62d28db2c3d8bcbe3ee
Stacy Turukin <ohnotredamn@gmail.com>
2023-07-10 06:30:39 +0300

    Задача по нахождению колличества четных чисел в массиве

---
+++ dz_task_34/Program.cs
@@ -0,0 +1,47 @@
+﻿/* Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+
+int[] FillArray(int length)
+{
+    int[] result = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        result[i] = new Random().Next(100, 1000);
+    }
+    return result;
+}
+
+void printArray(int[] arrayToPrint)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < arrayToPrint.Length; i++)
+    {
+        Console.Write(arrayToPrint[i]);
+        if (i != arrayToPrint.Length - 1)
+        {
+            Console.Write(",  ");
+        }
+    }
+    Console.WriteLine("] ");
+}
+
+int[] randomArray = FillArray(15);
+printArray(randomArray);
+
+int countOfEvenNumber(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count+=1;
+        }
+    }
+    return count;
+}
+
+//int result = countOfEvenNumber(randomArray);
+Console.WriteLine($"Колличество чётных чисел - {countOfEvenNumber(randomArray)}");
