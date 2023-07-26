60ab3181e0ebe63b08724c803cb6dac16779eff8
Stacy Turukin <ohnotredamn@gmail.com>
2023-07-26 16:50:52 +0300

    Задача с рекурсией выод чисел от n  до 1

---
+++ dz_task_64/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Задача 64: Задайте значение N. Напишите программу, 
+которая выведет все натуральные числа в промежутке от N до 1.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1" */
+
+void NaturalNumbers(int value, int count = 1)
+{
+    if (value < count)
+        return;
+    else
+    {
+        Console.Write(value + " ");
+        NaturalNumbers(value - 1, count);
+    }
+}
+NaturalNumbers(8);
