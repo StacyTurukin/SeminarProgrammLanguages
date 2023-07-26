6911af821ca9adb8fd02b152d06ee0bcab5620eb
Stacy Turukin <ohnotredamn@gmail.com>
2023-07-26 16:51:54 +0300

    Задача с рекурсией, сумма натуральных чисел от m до n

---
+++ dz_task_66/Program.cs
@@ -0,0 +1,12 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, 
+которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+int SumOfNum(int M, int N)
+{
+    if (M == N) return M;
+    return M + SumOfNum(M + 1, N);
+}
+int result = SumOfNum(1, 15);
+System.Console.WriteLine(result);
